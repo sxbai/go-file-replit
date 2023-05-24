ec3fba1fc41d2e8141b530733a65cc5726f6e47e
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-05-24 17:56:50 +0800

    Go-File-Replit一键搭建脚本

---
+++ install.sh
@@ -0,0 +1,16 @@
+echo "Go-File-Replit一键搭建脚本"
+echo "脚本作者：舒夏"
+echo "GitHub开源地址：https://github.com/sxbai/go-file-replit"
+nix-env -iA nixpkgs.wget
+mkdir build
+cd build
+wget -O .replit https://github.com/sxbai/go-file-replit/raw/master/.replit
+wget -O replit.nix https://github.com/sxbai/go-file-replit/raw/master/replit.nix
+cd ..
+wget -O main.sh https://github.com/sxbai/go-file-replit/raw/master/main.sh
+cp -r build/.replit . && cp -r build/replit.nix .
+echo "Go-File-Replit一键搭建脚本"
+echo "脚本作者：舒夏"
+echo "GitHub开源地址：https://github.com/sxbai/go-file-replit"
+echo "点击Run按钮，启动Go-File项目! 使用愉快!!!"
+rm -rf build/ && rm -rf install.sh
